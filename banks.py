33d5d410c69b5b3c370723f3557469d8c32b17ab
Kristaps Kozlovskis <108718585+devkristaps@users.noreply.github.com>
2022-09-18 18:04:41 +0300

    Update banks.py

---
+++ banks.py
@@ -0,0 +1,22 @@
+class Banks:
+
+    available_banks = {
+        "HSBC Holdings": 4,
+        "Lloyds Banking Group": 4,
+        "NatWest Group": 5,
+        "Barclays": 3
+    }
+
+    def __init__(self):
+        pass
+
+
+    def __repr__():
+        print("{:<26} {:<15}".format('Bank','Interest Rate'))
+        print('----------------------------------------')
+        for key, value in Banks.available_banks.items():
+            value = str(value) + '%'
+            print("{:<26} {:<15}".format(key, value))
+
+
+Banks.__repr__()
