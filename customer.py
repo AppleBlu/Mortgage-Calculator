1c5b4aeb2dd4acc3e596f256a3a8bd41415cfccf
Negative <79591062+AppleBlu@users.noreply.github.com>
2022-09-18 16:20:55 +0100

    Adding repayment _time

--- customer.py
+++ customer.py
@@ -9,9 +9,11 @@ class Customer():
     def user_inputs():
         down_payment = input('How much would you like to give as a down payment? \nPlease enter here: ')
         purchase_price = input('What is the value of the property you are trying to purchase? \nPlease enter here: ')
+        repayment_time = input('How long do you need to repay the loan with interest? (in months) \nPlease enter here: ')
 # test        
         # print(down_payment)
         # print(purchase_price)
+        # print(repayment_time)
 
 # test
 # creating a class instance
