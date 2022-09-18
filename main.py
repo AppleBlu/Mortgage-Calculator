e5a392b0fe189fad3265a0bc9e3fe93d46d78083
Kristaps Kozlovskis <108718585+devkristaps@users.noreply.github.com>
2022-09-18 16:16:17 +0300

    Added the main menu with options
    Added the initial look of the main menu with 4 options. We need to review the options before moving with merging I would say. Agree upon what we are going to do inside the menu.

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
 import art
+import customer
 
-print(art.art)
+is_on = True
+
+def application():
+    while is_on:
+        print(art.art)
+        print("-------------------------------------------------")
+        print("Please, choose the option")
+        print("1. Calculate the mortgage")
+        print("2. Available banks and rates")
+        print("3. Contact support")
+        print("4. Exit")
+        print("-------------------------------------------------")
+
+        customer_option = input("Enter your option here: ")
+
+        if customer_option == '1':
+            pass
+        elif customer_option == '2':
+            pass
+        elif customer_option == '3':
+            pass
+        elif customer_option == '4':
+            print("Thank you and hope to see you soon!")
+            exit()
+        else:
+            print("Wrong input")
+            exit()
+
+
+application()
