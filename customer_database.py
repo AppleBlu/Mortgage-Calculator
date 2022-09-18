eb08b949be81ec03c2174dffd0471aa11e48e862
Kristaps Kozlovskis <108718585+devkristaps@users.noreply.github.com>
2022-09-18 21:20:04 +0300

    Update customer_database.py

--- customer_database.py
+++ customer_database.py
@@ -1,2 +1,7 @@
+from time import sleep
 class CustomerDatabase:
-    pass
+    customers = { 0: {'name': 'Dummy', 'surname': 'Joe', 'email': 'joe@dummy.com', 'phone': '00000000',
+         'username': 'dummyjoe', 'password': 'dumbpass123'},
+    }
+
+    
