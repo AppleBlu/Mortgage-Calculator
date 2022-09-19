904c52efd7977ce10522842a8cbee30cc43b1455
Tom <79591062+AppleBlu@users.noreply.github.com>
2022-09-19 18:10:24 +0100

    v0.5.5

--- customer.py
+++ customer.py
@@ -1,5 +1,6 @@
 # create a class for the customer
 class Customer():
+    bank_selection = ''
 
 # making a constructor
     def __init__(self):
@@ -10,16 +11,10 @@ class Customer():
         down_payment = input('How much would you like to give as a down payment? \nPlease enter here: ')
         purchase_price = input('What is the value of the property you are trying to purchase? \nPlease enter here: ')
         repayment_time = input('How long do you need to repay the loan with interest? (in months) \nPlease enter here: ')
-        bank_selection = input('What bank would you like to apply for a mortgage with? \nPlease enter here: ')
-# test        
-        # print(down_payment)
-        # print(purchase_price)
-        # print(repayment_time)
-        # print(bank_selection)
+        bank_selection_input = input('What bank would you like to apply for a mortgage with? \nPlease enter here: ')
 
-# test
-# creating a class instance
-# customer1 = Customer.user_inputs()
+        Customer.bank_selection += bank_selection_input 
 
-# printing class instance
-# print(customer1)
+test1 = Customer
+print(test1.user_inputs())
+print(test1.bank_selection)
